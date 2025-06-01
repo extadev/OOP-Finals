47bf86b0e096843ab0bfd03297196f11913eb408
Command Line Beginner <legend.proyt45@gmail.com>
2025-06-01 19:38:47 +0800

    first commit

---
+++ Tasks.java
@@ -0,0 +1,17 @@
+public class Tasks {
+
+    private String tasks;
+
+    Tasks(String newTask) {
+        tasks = newTask;
+    }
+
+    // public void Tasks(String newTask) {
+    //     tasks = newTask;
+    // }
+
+    @Override
+    public String toString() {
+        return tasks;
+    }
+}
---
+++ finals.java
@@ -0,0 +1,130 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import java.time.LocalTime;
+import javafx.geometry.Pos;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+import java.util.ArrayList;
+
+
+public class finals extends Application {
+    
+    ArrayList<Tasks> task_list = new ArrayList();
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage ps) {
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.TOP_LEFT);
+        
+        HBox hb1 = new HBox();
+        hb1.setAlignment(Pos.TOP_LEFT);
+        hb1.setPrefWidth(1000);  
+        hb1.setPrefHeight(32);
+        hb1.setSpacing(5);
+
+
+        HBox hb2 = new HBox(); 
+        hb2.setAlignment(Pos.TOP_LEFT);
+        hb2.setPrefWidth(1000);  
+        hb2.setPrefHeight(100);
+        hb2.setSpacing(5);
+
+        HBox hb3 = new HBox(); 
+        hb3.setAlignment(Pos.TOP_LEFT);
+        hb3.setPrefWidth(1000);  
+        hb3.setPrefHeight(20);
+        hb3.setSpacing(5);
+
+        HBox hb4 = new HBox(); 
+        hb3.setAlignment(Pos.TOP_LEFT);
+        hb3.setPrefWidth(1000);  
+        hb3.setPrefHeight(20);
+        hb3.setSpacing(5);
+
+        VBox vbmain = new VBox();
+        vbmain.setAlignment(Pos.TOP_LEFT);
+        vbmain.setPrefHeight(500);  
+        vbmain.setPrefWidth(700);
+        vbmain.setSpacing(5);
+
+        
+        // LABELS
+        //--------------------------------------------
+        // - hb1 (labels)
+        int fontSize = 13;
+        Label lbltask = new Label("Task: ");
+        lbltask.setFont(new Font(fontSize));
+
+        Label lbldate = new Label("             Date: ");
+        lbldate.setFont(new Font(fontSize));
+
+        Label lblindex = new Label("             Index: ");
+        lblindex.setFont(new Font(fontSize));
+
+        // BUTTONS
+        // - hb2 (buttons)
+        //--------------------------------------------
+        Button btnadd = new Button("Add");
+        Button btndel = new Button("Delete");
+        Button btnmod = new Button("Modify");
+        Button btnsort = new Button("Sort by Date");
+        //
+
+
+        // TXT FIELDS
+        TextField tftask = new TextField();
+        tftask.setPromptText("Task description");
+        Scene scn1 = new Scene(root);
+        
+
+        // BTN METHODS
+        btnadd.setOnAction(e -> {
+            Tasks addTask = new Tasks(tftask.getText());
+            task_list.add(addTask);
+            System.out.println(task_list);
+        });
+
+        // --- needs indexing to specify which index to remove (as shown:)
+        // btndel.setOnAction(e -> {
+        //     Tasks addTask = new Tasks(tftask.getText());
+        //     task_list.add(addTask);
+        //     System.out.println(task_list);
+        // });
+
+        
+        
+        //
+
+        // DATE PICKER
+        DatePicker datepick = new DatePicker();
+        //
+
+
+        // DISPLAYING
+        // - hb3 (title area)
+        Label lbltitle = new Label("Tasks:" + "Noob");
+        // - hb4 (text area/display)
+        Label lblarea = new Label("1. First fucking task");
+
+        hb1.getChildren().addAll(lbltask, tftask, lbldate, datepick, lblindex);
+        hb2.getChildren().addAll(btnadd, btndel, btnmod, btnsort);
+        hb3.getChildren().addAll(lbltitle);
+        hb4.getChildren().addAll(lblarea);
+        vbmain.getChildren().addAll(hb1, hb2, hb3, hb4);
+        root.getChildren().addAll(vbmain);
+        root.setPadding(new Insets(20));
+        ps.setTitle("To-Do List with Dates");
+        ps.setMaximized(false);
+        ps.setScene(scn1);
+        ps.show();
+    }
+}
